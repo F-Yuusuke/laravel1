4bb271af248f523cba9e417b1739e66f887c9270
Ｆ.Ｙuusuke <>
2019-12-10 16:04:51 +0800

    DBにテーブルを作成
    ターミナルでファイルを追加したり、その中にコードを追加した

---
+++ Diary/app/Http/Controllers/DiaryController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DiaryController extends Controller
+{
+   // 追加
+    public function index()
+    {
+        return 'お腹すいた';
+    }
+}
