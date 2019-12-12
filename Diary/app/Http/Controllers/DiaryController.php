02c104c4d1a5d33a99c04723cad71f3995c791fa
Ｆ.Ｙuusuke <>
2019-12-12 14:21:53 +0800

    削除機能追加
    画面に削除ボタンを作成クリックすると削除されてその後トップページに戻る様にした

--- Diary/app/Http/Controllers/DiaryController.php
+++ Diary/app/Http/Controllers/DiaryController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Diary; // App/Diaryクラスを使用する宣言
 use Illuminate\Http\Request;
+use App\Http\Requests\CreateDiary; // 追加
 
 class DiaryController extends Controller
 {
@@ -12,7 +13,8 @@ class DiaryController extends Controller
         //diariesテーブルのデータを全件取得
         //useしてるDiaryのallメソッドを実施
         //all()はテーブルのデータを全て取得するメソッド
-        $diaries = Diary::all();
+        // $diaries = Diary::all();
+        $diaries = Diary::orderBy('id', 'desc')->get();
 
         // dd($diaries);  //var_dump()とdie()を合わせたメソッド。変数の確認 + 処理のストップ
         return view('diaries.index',['diaries' => $diaries]);
@@ -24,7 +26,7 @@ class DiaryController extends Controller
         return view('diaries.create');
     }
 
-    public function store(Request $request)
+    public function store(CreateDiary $request)
 {
     $diary = new Diary(); //Diaryモデルをインスタンス化
     // dd('ここに保存処理');
@@ -32,6 +34,17 @@ class DiaryController extends Controller
     $diary->title = $request->title;
     $diary->body = $request->body;
     $diary->save();
+    return redirect()->route('diary.index');
+}
+
+ public function destroy(int $id)
+{
+    //Diaryモデルを使用して、diariesテーブルから$idと一致するidをもつデータを取得
+    $diary = Diary::find($id); 
+
+    //取得したデータを削除
+    $diary->delete();
+
     return redirect()->route('diary.index');
 }
 }
