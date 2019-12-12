872212f59ac88b2864d3d1a6b2e58d130b14f759
Ｆ.Ｙuusuke <>
2019-12-12 15:57:29 +0800

    復習しよ
    共通部分を記述するファイル(テンプレート)の作成

--- Diary/resources/views/diaries/index.blade.php
+++ Diary/resources/views/diaries/index.blade.php
@@ -1,13 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/app.css">
-    <title>一覧表示画面</title>
-</head>
-<body>
+@extends('layout')
+
+@section('title')
+一覧
+@endsection
+
+
+@section('content')
 <a href="{{ route('diary.create') }}" class="btn btn-primary btn-block">
     新規投稿
 </a>
@@ -16,13 +14,14 @@
         <p>{{ $diary->title }}</p>
         <p>{{ $diary->body }}</p>
         <p>{{ $diary->created_at }}</p>
-        <p>{{ $diary->created_at }}</p>
-        <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="POST" class="d-inline">
+        <a class="btn btn-success" href="{{ route('diary.edit', ['id' => $diary->id]) }}">編集</a>
+
+        <form action="{{ route('diary.destroy', ['id' => $diary->id]) }}" method="post" class="d-inline">
             @csrf
             @method('delete')
-        <button class="btn btn-danger">削除</button>
+            <button class="btn btn-danger">削除</button>
+        </form>
     </form>
     </div>
-@endforeach  
-</body>
-</html>
+@endforeach 
+@endsection 
