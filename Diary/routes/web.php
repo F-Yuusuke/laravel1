7301eb7f26f174abf0982a245c41808fe3f72c73
Ｆ.Ｙuusuke <>
2019-12-16 14:33:24 +0800

    12月16日
    ユーザー認証機能追加

--- Diary/routes/web.php
+++ Diary/routes/web.php
@@ -16,6 +16,22 @@
 // routes/web.php
 Route::get('/', 'DiaryController@index')->name('diary.index');
 
+
+// 一覧以外のページはログインしていないと表示(実行)できないように変更
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('diary/create', 'DiaryController@create')->name('diary.create');
+    Route::post('diary/create', 'DiaryController@store')->name('diary.create');
+    
+    Route::get('diary/{diary}/edit', 'DiaryController@edit')->name('diary.edit');
+    Route::put('diary/{diary}/update', 'DiaryController@update')->name('diary.update');
+    
+    Route::delete('diary/{diary}/delete', 'DiaryController@destroy')->name('diary.destroy');    
+});
+
+Auth::routes();
+
+
+
 Route::get('diary/create', 'DiaryController@create')->name('diary.create'); // 投稿画面
 Route::post('diary/create', 'DiaryController@store')->name('diary.store'); // 保存処理
 Route::delete('diary/{id}/delete', 'DiaryController@destroy')->name('diary.destroy'); // 削除処理
@@ -26,3 +42,8 @@ Route::put('diary/{id}/update', 'DiaryController@update')->name('diary.update');
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Auth::routes();
+
+// 必要ないため削除
+// Route::get('/home', 'HomeController@index')->name('home');
