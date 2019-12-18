d700820c737481e5ff91f59b45ca61a26433f3ce
Ｆ.Ｙuusuke <>
2019-12-18 18:58:35 +0800

    12月18日　いいね機能の作成
    いいねされたデータを保存するためのテーブルを作成
    中間テーブルを作る
    いいね機能と解除ができるようにコードを書く
    いいね!ボタンをフォントアンセムから持ってくる
    削除　投稿ボタンの下にいいねボタンを追加
    画面遷移したくないのでAjaxを使う
    そのためにAjaxとJavaScriptを読み込めるようにして
    新しくジャバスクリプトのファイルを作る

--- Diary/routes/web.php
+++ Diary/routes/web.php
@@ -22,7 +22,9 @@ Route::group(['middleware' => 'auth'], function() {
     Route::get('diary/{diary}/edit', 'DiaryController@edit')->name('diary.edit');
     Route::put('diary/{diary}/update', 'DiaryController@update')->name('diary.update');
     
-    Route::delete('diary/{diary}/delete', 'DiaryController@destroy')->name('diary.destroy');    
+    Route::delete('diary/{diary}/delete', 'DiaryController@destroy')->name('diary.destroy'); 
+    Route::post('diary/{id}/like', 'DiaryController@like');
+    Route::post('diary/{id}/dislike', 'DiaryController@dislike');   
 });
 
 Route::get('diary/create', 'DiaryController@create')->name('diary.create'); // 投稿画面
